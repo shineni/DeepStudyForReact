0a90ed5ca751428f14c4f10792f9e1b40da73932
dingxiaowei <1213250243@qq.com>
2019-11-19 18:43:29 +0800

    1

--- my-app/src/components/app.jsx
+++ my-app/src/components/app.jsx
@@ -4,11 +4,22 @@ import Search from "./search";
 import Main from "./main";
 
 export default class App extends Component{
+
+    state = {
+        searchName : '',
+    }
+
+    setSearchName = (searchName) => {
+        // this.setSearchName = searchName //这样更新是不行的
+        //更新状态
+        this.setState({searchName})
+    }
+
     render(){
         return(
             <div className="container">
-                <Search/>
-                <Main/>
+                <Search setSearchName={this.setSearchName}/>
+                <Main searchName={this.state.searchName}/>
             </div>
         )
     }
--- my-app/src/components/main.jsx
+++ my-app/src/components/main.jsx
@@ -1,15 +1,70 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import search from "./search";
+import axios from 'axios'
+
 export default class Main extends Component{
+
+    static propTypes = {
+        searchName:PropTypes.string.isRequired
+    }
+
+    state = {
+        initView : true,
+        loading : false,
+        users : null,
+        errorMsg : null
+    }
+
+    //当组件接收到新的属性时回调
+    componentWillReceiveProps(newProps, nextContext) {
+        const {searchName} = newProps
+        //更新状态(请求中)
+        this.setState({
+            initView :false,
+            loading:true
+        })
+        //发送ajax请求
+        const url = `https://api.github.com/search/users?q=${searchName}`
+        axios.get(url).then(response => {
+            //得到相应数据
+            const result = response.data
+            console.log(resule)
+            const users = result.items.map(item=>({name:item.login}))
+            //更新状态(成功)
+
+        }).catch(error => {
+            //更新状态(失败)
+
+        })
+    }
+
     render(){
-        return(
-            <div className="row">
-                <div className="card">
-                    <a href="https://github.com/reactjs" target="_blank">
-                        <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: 100}}/>
-                    </a>
-                    <p className="card-text">reactjs</p>
+        const {initView,loading,users, errorMsg} = this.state
+        const {searchName} = this.props
+        console.log('render()',searchName)
+
+        if(initView) {
+            return <h2>请输入关键词进行搜索{searchName}</h2>
+        }else if(loading){
+            return <h2>正在请求中...</h2>
+        }else if(errorMsg){
+            return <h2>{errorMsg}</h2>
+        }else {
+            return(
+                <div className="row">
+                    {
+                        users.map((user,index)=>(  //{}里面要return
+                            <div className="card">
+                                <a href={user.url} target="_blank">
+                                    <img src={user.avatarUrl} style={{width: 100}}/>
+                                </a>
+                                <p className="card-text">{user.name}</p>
+                            </div>
+                        ))
+                    }
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
